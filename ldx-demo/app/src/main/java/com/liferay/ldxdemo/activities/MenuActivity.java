f2873e1db2726486037846011f3815eb4d5d9120
Javier Gamarra <nhpatt@gmail.com>
2016-03-17 13:36:48 +0100

    remove menu

--- ldx-demo/app/src/main/java/com/liferay/ldxdemo/activities/MenuActivity.java
+++ ldx-demo/app/src/main/java/com/liferay/ldxdemo/activities/MenuActivity.java
@@ -142,28 +142,6 @@ public class MenuActivity extends PushScreensActivity implements FragmentLoaded,
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.nav_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
